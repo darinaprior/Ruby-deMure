50e09a168f42136f19d29d970033455ce3087730
Darina Prior <darinaprior@gmail.com>
2012-04-14 21:53:56 +0100

    Collection page - breadcrumbs and 1st line overlapping Fix the CSS for the breadcrumbs so that it doesn't bring the main content up 15px anymore; Also quickly add "marketing" email address to contact page and fix up code format a bit; This fixes #11.

--- sandbox/Include/contact_details.php
+++ sandbox/Include/contact_details.php
@@ -1,10 +1,10 @@
 <?
-$g_sAddressMain				= "Ruby deMure<br/>4A Richmond Avenue<br/>Monkstown<br/>Co. Dublin<br/>Ireland";
-$g_sPhoneMain				= "+353 87 997 9323";
-$g_sEmailMain				= "info@rubydemure.com";
-
-$g_sEmailAdmin				= "admin@rubydemure.com";
-$g_sEmailComments			= "comments@rubydemure.com";
-$g_sEmailCustomerService	= "customer.service@rubydemure.com";
-$g_sEmailRuby				= "ruby@rubydemure.com";
+$g_sAddressMain = "Ruby deMure<br/>4A Richmond Avenue<br/>Monkstown<br/>Co. Dublin<br/>Ireland";
+$g_sPhoneMain = "+353 (0)87 997 9323";
+$g_sEmailMain = "info@rubydemure.com";
+$g_sEmailAdmin = "admin@rubydemure.com";
+$g_sEmailComments = "comments@rubydemure.com";
+$g_sEmailCustomerService = "customer.service@rubydemure.com";
+$g_sEmailRuby = "ruby@rubydemure.com";
+$g_sEmailMarketing = "marketing@rubydemure.com";
 ?>
